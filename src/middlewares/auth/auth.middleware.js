0aa3e5c58065f354aaf7c1f49b26374336f5b21d
Daniel Bautista <danibaufu@gmail.com>
2024-12-22 01:36:51 -0600

    AUTH
    implement auth by JWT -> return token in headers and do not save nothing in server side

---
+++ src/middlewares/auth/auth.middleware.js
@@ -0,0 +1,19 @@
+import { checkToken } from "@helpers/auth.helper.js";
+
+const verifyTokenTimeout = async (req, res, next) => {
+  try {
+    // Extraer el token del encabezado Authorization
+    const token = req.headers["authorization"]?.split(" ")[1]; // Asume formato Bearer <token>
+    if (!token) throw new Error("No token provided");
+
+    // Verificar el token
+    const decoded = checkToken(token); 
+    req.user = decoded.payload.user;
+
+    next();
+  } catch (error) {
+    res.status(401).send(`Unauthorized: ${error.message}`);
+  }
+};
+
+export { verifyTokenTimeout };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,9 @@
 import express from "express"; // import express
 import domain_router from "./domain/domain.routes.js";
 import model_status_router from "./model_states/model_status.routes.js";
+import auth_router from "./auth/auth.routes.js";
+
+import { verifyTokenTimeout } from "@middlewares/auth/auth.middleware.js";
 
 const bundled_router = express.Router(); // create a router
 
@@ -8,7 +11,8 @@ bundled_router.get("/", (req, res) => {
   res.send("API works!");
 });
 
-bundled_router.use("/states", model_status_router);
-bundled_router.use(domain_router);
+bundled_router.use("/auth", auth_router);
+bundled_router.use("/states", verifyTokenTimeout, model_status_router);
+bundled_router.use(verifyTokenTimeout, domain_router);
 
 export default bundled_router;
